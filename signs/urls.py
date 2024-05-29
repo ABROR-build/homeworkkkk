27ba9c167fb8750202e7b9b932104177568abf9c
ABROR-build <142251145+ABROR-build@users.noreply.github.com>
2024-05-29 13:01:14 +0500

    added detail and update

--- signs/urls.py
+++ signs/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('list-categories', views.CategoryListAPIView.as_view(), name='list-categories'),
-    path('list-signs', views.SignsListAPIView.as_view(), name='list-signs')
+    path('categories-list/', views.CategoryListAPIView.as_view(), name='list-categories'),
+    path('signs-list/', views.SignsListAPIView.as_view(), name='list-signs'),
+    path('signs-detail/<int:pk>/', views.SignsDetailView.as_view(), name='detail-signs'),
+    path('signs-delete/<int:pk>/', views.SignsDeleteView.as_view(), name='delete-signs'),
 ]
--- signs/views.py
+++ signs/views.py
@@ -1,6 +1,10 @@
+# rest modules
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+
+# local modules
 from . import serializers
 from . import models
 
@@ -27,3 +31,43 @@ class SignsListAPIView(APIView):
             'status_code': status.HTTP_200_OK
         }
         return Response(serializer_data)
+
+
+class SignsDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            signs = models.Signs.objects.get(pk=pk)
+            serializer = serializers.SignsSerializers(signs)
+            serializer_data = {
+                'sign': serializer.data,
+                'status': 'success',
+                'status_code': status.HTTP_200_OK
+            }
+        except Exception as exp:
+            serializer_data = {
+                'Error': str(exp) + f" Moron item with id-{pk} is not there",
+                'status': 'ERROR',
+                'status code': status.HTTP_404_NOT_FOUND,
+            }
+        finally:
+            return Response(serializer_data)
+
+
+class SignsDeleteView(APIView):
+    def delete(self, request, pk):
+        try:
+            signs = models.Signs.objects.get(pk=pk)
+            signs.delete()
+            serializer_data = {
+                'sign': f'item with id-{pk} is deleted',
+                'status': 'success',
+                'status_code': status.HTTP_200_OK
+            }
+        except Exception as exp:
+            serializer_data = {
+                'Error': str(exp) + f" Moron you can't delete item with id-{pk} because it is not there",
+                'status': 'ERROR',
+                'status code': status.HTTP_404_NOT_FOUND,
+            }
+        finally:
+            return Response(serializer_data)
