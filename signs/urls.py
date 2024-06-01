34ee7e92e431e45c6d9ff3d63ee9a0bc6221ea18
ABROR-build <142251145+ABROR-build@users.noreply.github.com>
2024-06-01 14:55:37 +0500

    new

--- signs/urls.py
+++ signs/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('categories-list/', views.CategoryListAPIView.as_view(), name='list-categories'),
-    path('signs-list/', views.SignsListAPIView.as_view(), name='list-signs'),
-    path('signs-detail/<int:pk>/', views.SignsDetailView.as_view(), name='detail-signs'),
-    path('signs-delete/<int:pk>/', views.SignsDeleteView.as_view(), name='delete-signs'),
-    path('signs-put/<int:pk>/', views.SignsPutView.as_view(), name='put-signs'),
-    path('signs-create/', views.SignsCreateView.as_view(), name='create-signs'),
+    path('signs-create/', views.CreateApiView.as_view(), name='Create API'),
+    path('signs-get/', views.get, name = 'get')
 ]
--- signs/views.py
+++ signs/views.py
@@ -2,120 +2,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework.decorators import api_view
 
 # local modules
 from . import serializers
 from . import models
 
 
-class CategoryListAPIView(APIView):
-    def get(self, request):
-        category = models.Categories.objects.all()
-        serializer = serializers.CategorySerializers(category, many=True)
-        serializer_data = {
-            'category': serializer.data,
-            'status': 'success',
-            'status_code': status.HTTP_200_OK
-        }
-        return Response(serializer_data)
+class CreateApiView(ListCreateAPIView):
+    queryset = models.Signs.objects.all()
+    serializer_class = serializers.SignsSerializers
 
 
-class SignsListAPIView(APIView):
-    def get(self, request):
-        signs = models.Signs.objects.all()
-        serializer = serializers.SignsSerializers(signs, many=True)
-        serializer_data = {
-            'sign': serializer.data,
-            'status': 'success',
-            'status_code': status.HTTP_200_OK
-        }
-        return Response(serializer_data)
-
-
-class SignsDetailView(APIView):
-    def get(self, request, pk):
-        try:
-            signs = models.Signs.objects.get(pk=pk)
-            serializer = serializers.SignsSerializers(signs)
-            serializer_data = {
-                'sign': serializer.data,
-                'status': 'success',
-                'status_code': status.HTTP_200_OK
-            }
-        except Exception as exp:
-            serializer_data = {
-                'Error': str(exp) + f" Moron item with id-{pk} is not there",
-                'status': 'ERROR',
-                'status code': status.HTTP_404_NOT_FOUND,
-            }
-        finally:
-            return Response(serializer_data)
-
-
-class SignsDeleteView(APIView):
-    def delete(self, request, pk):
-        try:
-            signs = models.Signs.objects.get(pk=pk)
-            signs.delete()
-            serializer_data = {
-                'sign': f'item with id-{pk} is deleted',
-                'status': 'success',
-                'status_code': status.HTTP_200_OK
-            }
-        except Exception as exp:
-            serializer_data = {
-                'Error': str(exp) + f" Moron you can't delete item with id-{pk} because it is not there",
-                'status': 'ERROR',
-                'status code': status.HTTP_404_NOT_FOUND,
-            }
-        finally:
-            return Response(serializer_data)
-
-
-class SignsCreateView(APIView):
-    def post(self, request):
-        serializer = serializers.SignsSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'sign': serializer.data,
-                'status': 'success',
-                'status_code': status.HTTP_201_CREATED
-            })
-        else:
-            return Response({
-                'errors': serializer.errors,
-                'status': 'error',
-                'status_code': status.HTTP_400_BAD_REQUEST,
-            })
-
-
-class SignsPutView(APIView):
-    def put(self, request, pk):
-        serializer_data = {}  # Initialize to a default value
-        try:
-            signs = models.Signs.objects.get(pk=pk)
-            serializer = serializers.SignsSerializers(signs, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                serializer_data = {
-                    'sign': serializer.data,
-                    'status': 'success',
-                    'status_code': status.HTTP_200_OK
-                }
-            else:
-                serializer_data = {
-                    'errors': serializer.errors,
-                    'status': 'error',
-                    'status_code': status.HTTP_400_BAD_REQUEST,
-                }
-
-        except Exception as exp:
-            serializer_data = {
-                'Error': str(exp),
-                'status': 'ERROR',
-                'status code': status.HTTP_404_NOT_FOUND,
-            }
-        finally:
-            return Response(serializer_data)
+@api_view(['GET'])
+def get(self):
+    signs = models.Signs.objects.all()
+    serializer = serializers.SignsSerializers(signs, many=True)
+    serializer_data = {
+        'sign': serializer.data,
+        'status': 'success',
+        'status_code': status.HTTP_200_OK
+    }
+    return Response(serializer_data)
